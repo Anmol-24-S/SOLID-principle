533ffc4e42fe5a4bf28e7183df9ef2b430af8808
Anmol Singh <bhamrahanmol100@gmail.com>
2025-05-07 09:30:47 +0530

    Updated Solid.java with SOLID principles

--- src/Demo/Solid.java
+++ src/Demo/Solid.java
@@ -1,5 +1,76 @@
 package Demo;
 
-public class Solid {
+// S - Single Responsibility Principle
+class Invoice {
+    private String item;
+    private int quantity;
 
+    public Invoice(String item, int quantity) {
+        this.item = item;
+        this.quantity = quantity;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
+
+// O - Open/Closed Principle
+abstract class InvoicePrinter {
+    public abstract void print(Invoice invoice);
+}
+
+class PDFInvoicePrinter extends InvoicePrinter {
+    public void print(Invoice invoice) {
+        System.out.println("Printing invoice to PDF for: " + invoice.getItem());
+    }
+}
+
+// L - Liskov Substitution Principle
+class EmailInvoicePrinter extends InvoicePrinter {
+    public void print(Invoice invoice) {
+        System.out.println("Emailing invoice for: " + invoice.getItem());
+    }
+}
+
+// I - Interface Segregation Principle
+interface Printer {
+    void print();
+}
+
+interface Scanner {
+    void scan();
+}
+
+class SimplePrinter implements Printer {
+    public void print() {
+        System.out.println("Printing document...");
+    }
+}
+
+// D - Dependency Inversion Principle
+class InvoiceService {
+    private InvoicePrinter printer;
+
+    public InvoiceService(InvoicePrinter printer) {
+        this.printer = printer;
+    }
+
+    public void processInvoice(Invoice invoice) {
+        printer.print(invoice);
+    }
+}
+
+// Main class
+public class Solid {
+    public static void main(String[] args) {
+        Invoice invoice = new Invoice("Book", 2);
+        InvoicePrinter printer = new PDFInvoicePrinter();
+        InvoiceService service = new InvoiceService(printer);
+        service.processInvoice(invoice);
+    }
 }
